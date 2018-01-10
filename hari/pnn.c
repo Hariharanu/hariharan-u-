f25cacc611078bb615a8398be05415dddbb596d0
hariharan <31499070+Hariharanu@users.noreply.github.com>
2018-01-10 16:43:36 +0530

    Create pnn.c

---
+++ hari/pnn.c
@@ -0,0 +1,17 @@
+#include<iostream.h>
+void  main()
+{ int i;
+cin>>i;
+if(i==0)
+{ 
+cout<<"positive number ";
+}
+ else if (x>0)
+ {
+  cout<<"postive number";
+ }
+else 
+{
+cout<<"negative number ";
+}
+}
