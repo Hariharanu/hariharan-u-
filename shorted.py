8a657d991bc7f3450b56266f1a174d91a2c3d31e
hariharan <31499070+Hariharanu@users.noreply.github.com>
2018-01-19 16:54:12 +0530

    Create shorted.py

---
+++ shorted.py
@@ -0,0 +1,8 @@
+a=[]
+mylist =input('Enter your list range : ')
+for i in range(mylist):
+    m=input("enter the list element:")
+    a.append(m)
+print("list element are",a)
+a.sort()
+print("shorted list is ",a)
