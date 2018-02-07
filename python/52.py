1499b8e68c534c4e8161c31e85a8707721718a8a
hariharan <31499070+Hariharanu@users.noreply.github.com>
2018-02-07 16:08:56 +0530

    Create 52.py

---
+++ python/52.py
@@ -0,0 +1,26 @@
+a=["zero","one","two","three","four","five","six","seven","eight","nine","ten"]
+n=input("enter the number:")
+map(int,n)
+for i in range(0,len(n)):
+    if int(n[i])==0: 
+        print(a[0])
+    if int(n[i])==1:
+        print(a[1])
+    if int(n[i])==2:
+        print(a[2])
+    if int(n[i])==3:
+        print(a[3])
+    if int(n[i])==4:
+        print(a[4])
+    if int(n[i])==5: 
+        print(a[5])
+    if int(n[i])==6:
+        print(a[6])
+    if int(n[i])==7:
+        print(a[7])
+    if int(n[i])==8:
+        print(a[8])
+    if int(n[i])==9:
+        print(a[9])
+
+
