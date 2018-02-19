eaff9dc5fc856c49f15bbf4b882e5dd8a4a809a2
hariharan <31499070+Hariharanu@users.noreply.github.com>
2018-02-19 21:32:31 +0530

    Create 81.c

---
+++ 81.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i ,j,k,l,m,n; clrscr();
+scanf("%d%d",&i,&j);
+scanf("%d%d",&k,&l);
+scanf("%d%d",&m,&n);
+printf("%d\n",-(i-j));
+printf("%d\n",-(k-l));
+printf("%d\n",-(m-n));
+
+getch();
+}
