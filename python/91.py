52e4f9672db54b754ee02e138ec59db4404e7ae1
hariharan <31499070+Hariharanu@users.noreply.github.com>
2018-02-21 16:35:52 +0530

    Create 91.py

---
+++ python/91.py
@@ -0,0 +1,4 @@
+length,wdith,heigth=input("enter length,wdith,heigth,:").split(" ")
+print("area of cuboid",(int(length)*int(wdith)))
+print("volume of cuboid",(int(length)*int(wdith)*int(heigth)))
+    
