50d1b45aa36705baae28909774d550e27d3a992f
hariharan <31499070+Hariharanu@users.noreply.github.com>
2018-02-23 21:11:30 +0530

    Create 92.py

---
+++ python/92.py
@@ -0,0 +1,11 @@
+n=int(input("enter the limit"))
+j=0
+if n<=4:
+    k=input("enter the number ")
+    for i in range(0,len(k)):
+        j=j+int(k[i])
+    print(j)
+else:
+    print("enter only",n,"numers")    
+
+    
