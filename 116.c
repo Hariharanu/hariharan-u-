ee0468e45d3bb3fea1204e499ea614f64fc86082
hariharan <31499070+Hariharanu@users.noreply.github.com>
2018-02-28 16:57:47 +0530

    Create 116.c

---
+++ 116.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main()
+{
+
+    int n,k,a[90],j=0;
+
+    printf("Enter the two numbers");
+    scanf("%d%d",&n,&k);
+    printf("%d%d",n,k) ;
+        
+     
+    
+     
+
+}
